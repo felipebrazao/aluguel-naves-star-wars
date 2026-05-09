56a6c4552fbf43b7cdd28316621c39d6a4a6e919
Felipe Brazao <felipebrazao@outlook.com>
2026-05-09 11:46:15 -0300

    Criação do projeto de front e backend

---
+++ backend/starship-rental/src/main/java/com/starwars/starshiprental/StarshipRentalApplication.java
@@ -0,0 +1,13 @@
+package com.starwars.starshiprental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarshipRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarshipRentalApplication.class, args);
+	}
+
+}
---
+++ backend/starship-rental/src/test/java/com/starwars/starshiprental/StarshipRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starwars.starshiprental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarshipRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
